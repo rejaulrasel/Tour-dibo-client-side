b6044179102103a5dcb0047c3a91cd9a36421b08
rejaulrasel <rejaulhasanrasel28@gmail.com>
2021-10-31 23:48:29 +0600

    added firebase deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/home'>
+            <Home></Home>
+          </Route>
+          <Route exact path='/login'>
+            <Login></Login>
+          </Route>
+          <Route exact path='*'>
+            <PageNotFound></PageNotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
