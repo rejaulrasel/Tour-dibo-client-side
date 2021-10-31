5a377c0bb116016631feb764eb016073ea78a65c
rejaulrasel <rejaulhasanrasel28@gmail.com>
2021-10-31 22:42:00 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuVrqujaI5zTVsHrckazDuu-UNaQ5RdvU",
+    authDomain: "tour-dibo.firebaseapp.com",
+    projectId: "tour-dibo",
+    storageBucket: "tour-dibo.appspot.com",
+    messagingSenderId: "828083682497",
+    appId: "1:828083682497:web:7fc568632b32e6963da840"
+  };
+ 
+  export default firebaseConfig;
