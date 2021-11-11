590f6c9e2f8ee62cd4e19cf725375e13c4ae60a2
rejaulrasel <rejaulhasanrasel28@gmail.com>
2021-11-11 23:30:09 +0600

    added header

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
+
+
+
 const Header = () => {
     const {user,logout} = useAuth();
     return (
-        <div>
-            <Link to='/home'>Home</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/home'>Home</Link>
-            <Link to='/home'>Home</Link>
-            <Link to='/home'>Home</Link>
-            {user.email &&
-            <button onClick={logout}>Logout</button>
-            }
-
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+                <Container>
+                    <Navbar.Brand href="/home">TOUR-DIBO</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                    <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About US</Nav.Link>
+                        <Nav.Link as={Link} to="/contact">Contact US</Nav.Link>
+                        {user?.email ?
+                            <div className="d-flex flex-row">
+                                <Nav.Link as={Link} to="/myOrders">My Orders</Nav.Link>
+                                <Nav.Link as={Link} to="/manageOrder">Manage Orders</Nav.Link>
+                                <Nav.Link as={Link} to="/addService">Add Service</Nav.Link>
+                                <button className="btn btn-danger" onClick={logout}>Logout</button>
+                            </div>
+                            :
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                        }
+                        {user.email && 
+                        <Navbar.Text>
+                        Signed in as: <img src={user?.photoURL} width="50px" className="img-fluid" style={{borderRadius:"50%"}} alt="" /> { user?.displayName}
+                    </Navbar.Text>}
+                        
+                    </Navbar.Collapse>
+                    
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
