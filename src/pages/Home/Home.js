da3757c451a7debc163a0af49a6986bcba81b019
rejaulrasel <rejaulhasanrasel28@gmail.com>
2021-11-16 00:21:15 +0600

    added partners

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap';
 import useAuth from '../../Hooks/useAuth';
+import Partners from '../../Partners/Partners';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import './Home.css'
@@ -13,6 +14,7 @@ const Home = () => {
         <div>
           <Banner></Banner>
           <Services></Services>
+        <Partners>  </Partners>
         </div>
     );
 };
